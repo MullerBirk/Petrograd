c16e896fbacfce951823cf630fbb92d0f925ea35
Muller Birk <MullerBirk@Mullers-MacBook-Air.local>
2017-02-23 10:51:57 +0100

    lod JSON produktliste and create template for each

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ window.addEventListener("load", siden_vises);
 function siden_vises(){
      console.log("siden vises")
 
-     vis_produkt();
+     //læs data (produktliste)
+     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", vis_produkt_liste)
+
 }
 
-function vis_produkt(){
+function vis_produkt_liste(listen){
+    console.table(listen);
+
+    listen.forEach(vis_produkt);
+}
+
+function vis_produkt(produkt){
+    console.log (produkt)
     //klon produkt_template
 
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
