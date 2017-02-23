da204e614550c0103c062ba962aa65e05e79027a
Muller Birk <MullerBirk@Mullers-MacBook-Air.local>
2017-02-23 11:54:37 +0100

    udsolgt fix

--- script.js
+++ script.js
@@ -20,20 +20,35 @@ function vis_produkt(produkt){
 
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     //indsæt data i klon
-    klon.querySelector(".data_navn").innerHTML = produkt.navn;
-    klon.querySelector(".data_pris").innerHTML = produkt.pris;
-    //background-image: url(imgs/medium/bochkarev-md.jpg);
+        //background-image: url(imgs/medium/bochkarev-md.jpg);
     var imgPath = produkt.billede;
     klon.querySelector('.data_pic').style.backgroundImage="url(imgs/medium/"+imgPath+"-md.jpg)";
 
-
-
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
 
     var rabatpris = Math.ceil(produkt.pris - (produkt.pris*produkt.rabatsats/100));
     klon.querySelector(".data_rabat").innerHTML = rabatpris;
 
+    if(produkt.udsolgt == false){
+        //produkt er ikke udsolgt
+        //udsolgt tekst fjernes
+        var udsolgttekst = klon.querySelector(".udsolgt");
+        udsolgttekst.parentNode.removeChild( udsolgttekst);
+    }
+    else{
+        var udsolgttekst = klon.querySelector(".udsolgt");
+        udsolgttekst.parentNode.removeChild( udsolgttekst);
+
+        var prisen = klon.querySelector(".pris");
+        prisen.parentNode.removeChild( prisen );
+        var rabatten = klon.querySelector(".rabat");
+        rabatten.parentNode.removeChild( rabatten );
+    }
+
     //append klon til produktliste
     document.querySelector(".produktliste").appendChild(klon);
+
 }
 
 
